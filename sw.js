c50b9d46a868a48b3278224cb86e05731a0e05b4
Emmanuel Obeng Sika <Shepherdaitimeless@gmail.com>
2026-04-18 21:49:44 -0400

    Update HTML and fixes

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 //  Updated: Status, Edit, Link Preview, Emoji, Forward
 // ══════════════════════════════════════════════════
 
-var CACHE_NAME = 'linkup-v29';
+var CACHE_NAME = 'linkup-v31';
 var APP_URL    = 'https://shepherdai007.github.io/linkup/';
 
 var PRECACHE_ASSETS = [
