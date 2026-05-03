5653072483d6eb2b4f0a9e5a05fcc8a18dab09fe
Shepherdai007 <Shepherdaitimeless@gmail.com>
2026-05-02 22:01:02 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 // ── 3. CACHE CONFIG ──
-var CACHE_NAME = 'linkup-v114';
+var CACHE_NAME = 'linkup-v123';
 var APP_URL    = 'https://linkup-chat-8b593.web.app';
 
 var PRECACHE_ASSETS = [
